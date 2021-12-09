8d63f1c2c5f449c63e14284d7caa1fcfe99edb75
Viola Boros Federico <violaboros.federico@osdbsesto>
2021-12-09 11:45:07 +0100

    create array studenti.json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { request } = require("express");
 var express = require("express");
 var apiServer = express();
+var fs = require("fs");
 
 var port = 3000;
 var host = "localhost";
@@ -23,7 +24,46 @@ apiServer.get("/mioNome", (request, response) => {
 });
 
 apiServer.get("/somma", (request, response) => {
-    response.send("il numero A è " + request.query.a);
-    response.send("il numero B è " + request.query.b);
-    response.send("il risultato è " + (request.query.a) + (request.query.b));
+    console.log("somma request", request);
+    //Per sommare due stringhe si usa - - 
+        //response.send("risultato = " + (request.query.a -(-request.query.b)));
+    //Alternativa più pulita per sommare le due somme
+    response.send("risultato = " + (parseInt(request.query.a) + parseInt(request.query.b)));
+});
+
+apiServer.get("/student", (request, response) => {
+    console.log("student id: ", request.query.id);
+    //leggere il file
+    fs.readFile("studenti.json", (err, data) => {
+        if(err) {
+            console.log("error: " + err);
+        } else {
+            var students = JSON.parse(data);
+            for(i = 0; i < students.length; i++){                
+                console.log("students"+students[i].id+" surname: " + students[i].surname);
+                console.log("students"+students[i].id+" name: " + students[i].name);
+            }
+        }
+    });
+    //prelevare l'oggetto con id 1
+    //send
+});
+
+apiServer.get("/newStudent", (request, response) => {
+    console.log("student id: ", request.query.id);
+    fs.writeFile("studenti.json", (err, data) => {        
+        if(err) {
+            console.log("error: " + err);
+        } else {
+            var students = JSON.parse(data);
+            //students.push(request.query.surname);
+            //students.push(request.query.name);
+            //students.push(request.query.id);
+            request.query.surname.push(student[request.query.id]);
+            request.query.name.push(student[request.query.id]);            
+        }
+    });
+
+    //http://localhost:3000/newStudent?surname=violaboros&name=federico&id=9
+
 });
